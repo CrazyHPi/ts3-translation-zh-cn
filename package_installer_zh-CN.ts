fd2a00cece4cfdd5310d40a3c7e80ed43f554a72
CrazyHPi <30518002+CrazyHPi@users.noreply.github.com>
2018-11-28 08:59:22 +0800

    Update package_installer_zh-CN.ts

--- package_installer_zh-CN.ts
+++ package_installer_zh-CN.ts
@@ -5,117 +5,119 @@
     <name>InstallDialog</name>
     <message>
         <source>TeamSpeak Add-On</source>
-        <translation type="unfinished"></translation>
+        <translation type="TeamSpeak插件"></translation>
     </message>
     <message>
         <source>Name:</source>
-        <translation type="unfinished"></translation>
+        <translation type="名字："></translation>
     </message>
     <message>
         <source>Type:</source>
-        <translation type="unfinished"></translation>
+        <translation type="类型："></translation>
     </message>
     <message>
         <source>Author:</source>
-        <translation type="unfinished"></translation>
+        <translation type="作者："></translation>
     </message>
     <message>
         <source>Version:</source>
-        <translation type="unfinished"></translation>
+        <translation type="版本："></translation>
     </message>
     <message>
         <source>Description:</source>
-        <translation type="unfinished"></translation>
+        <translation type="简介："></translation>
     </message>
     <message>
         <source>Platform:</source>
-        <translation type="unfinished"></translation>
+        <translation type="平台："></translation>
     </message>
     <message>
         <source>This add-on is not available for your platform.</source>
-        <translation type="unfinished"></translation>
+        <translation type="这个插件在你的平台上不可用"></translation>
     </message>
     <message>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation type="安装"></translation>
     </message>
     <message>
         <source>Abort</source>
-        <translation type="unfinished"></translation>
+        <translation type="放弃"></translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation type="错误"></translation>
     </message>
     <message>
         <source>Failed to read Add-On info.</source>
-        <translation type="unfinished"></translation>
+        <translation type="读取插件信息失败"></translation>
     </message>
     <message>
         <source>All</source>
-        <translation type="unfinished"></translation>
+        <translation type="全选"></translation>
     </message>
     <message>
         <source>Select TeamSpeak Client installation folder</source>
-        <translation type="unfinished"></translation>
+        <translation type="选择TeamSpeak客户端安装文件夹"></translation>
     </message>
     <message>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation type="警告"></translation>
     </message>
     <message>
         <source>&lt;p&gt;You are about to install a Plugin for your TeamSpeak Client.&lt;/p&gt;&lt;p&gt;Plugins should only be installed from trusted sources, as it is possible to inject malicious code into the TeamSpeak client with a plugin.&lt;/p&gt;&lt;p&gt;Do you trust this Plugin and want to proceed installing?&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="&lt;p&gt;你将要为你的TeamSpeak客户端安装插件。&lt;p&gt;&lt;p&gt;请安装从可信来源获得的插件因为插件可能往TeamSpeak客户端中加入木马代码。&lt;p&gt;&lt;p&gt;你信任这个插件并继续安装？"></translation>
     </message>
     <message>
         <source>No TeamSpeak 3 Client installation directory found.</source>
-        <translation type="unfinished"></translation>
+        <translation type="找不到TeamSpeak 3客户端安装目录"></translation>
     </message>
     <message>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation type="信息"></translation>
     </message>
     <message>
         <source>&lt;p&gt;Add-On successfully installed.&lt;br&gt;Do you want to activate this Add-On?&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Please restart your TeamSpeak Client.&lt;/strong&gt;&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="&lt;p&gt;插件安装成功。&lt;br&gt;你是否想激活此插件？&lt;/p&gt;&lt;p&gt;&lt;strong&gt;请重启你的TeamSpeak客户端。&lt;/strong&gt;&lt;/p&gt;"></translation>
     </message>
     <message>
         <source>Add-On successfully installed.</source>
-        <translation type="unfinished"></translation>
+        <translation type="插件安装成功。"></translation>
     </message>
     <message>
         <source>Failed to install Add-On.
 Do you want to retry as Administrator?</source>
-        <translation type="unfinished"></translation>
+        <translation type="插件安装失败。
+        你是否想以管理员权限再试一次？"></translation>
     </message>
     <message>
         <source>Failed to install Add-On.</source>
-        <translation type="unfinished"></translation>
+        <translation type="插件安装失败"></translation>
     </message>
     <message>
         <source>File not found: %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="找不到：%1"></translation>
     </message>
     <message>
         <source>Invalid zip file: %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="无效的zip文件： %1"></translation>
     </message>
     <message>
         <source>Failed to find temp directory</source>
-        <translation type="unfinished"></translation>
+        <translation type="找不到临时目录"></translation>
     </message>
     <message>
         <source>Failed to create temp path: %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="无法创建临时路径：%1"></translation>
     </message>
     <message>
         <source>Failed to extract: %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="提取文件失败：%1"></translation>
     </message>
     <message>
         <source>Failed to extract Add-On:
 %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="提取插件失败：
+        %1"></translation>
     </message>
 </context>
 </TS>
